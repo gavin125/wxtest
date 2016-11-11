6e60127c399f7f43f21a77787577589f39de31a8
gavin <278033412@qq.com>
2016-11-11 17:39:20 +0800

    首页API添加

--- pages/weather/weather.js
+++ pages/weather/weather.js
@@ -2,19 +2,21 @@
 //获取应用实例
 var app = getApp()
 Page({
-  data: {},
-  bindViewTap:function(){
-    console.log("111")
-  },
+  data:{
+    basic:"",
+    now:"",
+    suggestion:""
+  },//默认数据
   onLoad: function () {
-    console.log('onLoad')
-    var that = this
-    app.getdata(function(data){
-      //更新数据
-      that.setData(data)
-      console.log(data)
+    var that = this;
+    console.log(that);
+    app.getnow(function(d){
+      that.setData({basic:d.basic,now:d.now})//更新数据
     })
-  	
+    app.getsuggestion(function(d){
+      that.setData({suggestion:d.suggestion})//更新数据
+    })
+  	console.log(that.data)
   }
   
 })
