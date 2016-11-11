854ab273a67885b88586b20255761f7f13205cee
gavin <278033412@qq.com>
2016-11-11 18:08:38 +0800

    根据天气编号生成图标路径数据

--- app.js
+++ app.js
@@ -26,38 +26,7 @@ App({
   },
   globalData:{
     userInfo:null
-  },
-  //获取当前天气API
-  getnow:function(f){
-    wx.request({
-      url: 'https://free-api.heweather.com/v5/now', 
-      data: {
-        city:'CN101010100',
-        key:'01a7798b060b468abdad006ea3de4713'
-      },
-      header: {
-        'Content-Type': 'application/json'
-      },
-      success: function(res) {
-        f(res.data.HeWeather5[0]);
-      }
-    })
-  },
-  //获取生活指数API
-  getsuggestion:function(f){
-    wx.request({
-      url: 'https://free-api.heweather.com/v5/suggestion', 
-      data: {
-        city:'CN101010100',
-        key:'01a7798b060b468abdad006ea3de4713'
-      },
-      header: {
-        'Content-Type': 'application/json'
-      },
-      success: function(res) {
-        f(res.data.HeWeather5[0]);
-      }
-    })
   }
+  
 
 })
--- pages/weather/weather.js
+++ pages/weather/weather.js
@@ -10,13 +10,46 @@ Page({
   onLoad: function () {
     var that = this;
     console.log(that);
-    app.getnow(function(d){
+    that.getnow(function(d){
+      d.now.cond.src="http://files.heweather.com/cond_icon/"+d.now.cond.code+".png"
       that.setData({basic:d.basic,now:d.now})//更新数据
     })
-    app.getsuggestion(function(d){
+    that.getsuggestion(function(d){
       that.setData({suggestion:d.suggestion})//更新数据
     })
   	console.log(that.data)
+  },
+  //获取当前天气API
+  getnow:function(f){
+    wx.request({
+      url: 'https://free-api.heweather.com/v5/now', 
+      data: {
+        city:'CN101010100',
+        key:'01a7798b060b468abdad006ea3de4713'
+      },
+      header: {
+        'Content-Type': 'application/json'
+      },
+      success: function(res) {
+        f(res.data.HeWeather5[0]);
+      }
+    })
+  },
+  //获取生活指数API
+  getsuggestion:function(f){
+    wx.request({
+      url: 'https://free-api.heweather.com/v5/suggestion', 
+      data: {
+        city:'CN101010100',
+        key:'01a7798b060b468abdad006ea3de4713'
+      },
+      header: {
+        'Content-Type': 'application/json'
+      },
+      success: function(res) {
+        f(res.data.HeWeather5[0]);
+      }
+    })
   }
   
 })
