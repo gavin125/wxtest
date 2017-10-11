327d43717f1d8029fb443ecb03e0f91816dded03
gavin <278033412@qq.com>
2017-10-11 18:02:33 +0800

    根据API和开发工具更新适当调整

--- app.js
+++ app.js
@@ -1,17 +1,20 @@
 //app.js
 App({
-  //1、系统事件部分
+  //系统事件
   onLaunch: function () {//小程序初始化事件
     var that=this;
     //调用API从本地缓存中获取数据
     that.curid = wx.getStorageSync('curid') || that.curid;//API：获取本地缓存，若不存在设置为全局属性
     that.setlocal('curid', that.curid);//调用全局方法
   },
-  //2、自定义全局方法部分
+
+  /*******************************************************/
+
+  //自定义全局方法
   setlocal:function(id,val){
     wx.setStorageSync(id, val);//API：设置本地缓存
   },
-  //3、自定义全局属性部分
+  //自定义全局属性
   curid:"CN101010100",
   version:"1.0"
 })
